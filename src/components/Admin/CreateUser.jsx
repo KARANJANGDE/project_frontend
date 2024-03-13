b403f84571f9dcd2600d2c701eb3c1d8572a2ff2
karan <jangdekaran4@gmail.com>
2024-03-13 23:31:26 +0530

    createuser

--- src/components/Admin/CreateUser.jsx
+++ src/components/Admin/CreateUser.jsx
@@ -9,156 +9,168 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
-
 const CreateUser = () => {
+  const [selectedProjects, setSelectedProjects] = useState([]);
 
-    const navigate=useNavigate();
-    const [project, setProject] = useState([]);
-    const [role, setrole] = useState('')
-  
-    const getAllProject = async () => {
-      try {
-        const res = await axios.get("http://localhost:4000/api4/project");
-        console.log(res.data.data);
-        setProject(res.data.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const navigate = useNavigate();
+  const [project, setProject] = useState([]);
+  const [role, setrole] = useState("");
 
-    // const getrolebyid=async()=>{
-    //   try {
-    //     const res=await axios.get("http://localhost:4000/api3/role/65ad12f317279bae27eb5943")
-    //     console.log(res.data.data);
-    //     setrole(res.data.data._id);
-        
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // }
+  const getAllProject = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/api4/project");
+      console.log(res.data.data);
+      setProject(res.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // const getrolebyid=async()=>{
+  //   try {
+  //     const res=await axios.get("http://localhost:4000/api3/role/65ad12f317279bae27eb5943")
+  //     console.log(res.data.data);
+  //     setrole(res.data.data._id);
+
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  useEffect(() => {
+    getAllProject();
+    // getrolebyid();
+  }, []);
+
+  const { register, handleSubmit } = useForm();
+
+  const submitHandler = async (data) => {
+    try {
+      const object = {
+        FirstName: data.FirstName,
+        LastName: data.LastName,
+        UserEmail: data.UserEmail,
+        UserPass: data.UserPass,
+        ProjectID: selectedProjects,
+        role: "65ad12f317279bae27eb5943",
+        status: data.status,
+      };
+      console.log(object);
+      const res = await axios.post("http://localhost:4000/api/user", object);
+      console.log(res.data.data);
+      navigate("/admin/adminprofile");
+    } catch (error) {
+      console.log(
+        "Failed to submit project:",
+        error.response ? error.response.data : error.message
+      );
+    }
+  };
+  const handleProjectChange = (e) => {
+    const projectId = e.target.value;
+    const isChecked = e.target.checked;
   
-    useEffect(() => {
-      getAllProject();
-      // getrolebyid();
-    }, []);
-  
-    const { register, handleSubmit } = useForm();
-  
-    const submitHandler = async (data) => {
-      try {
-        const object={
-          FirstName:data.FirstName,
-          LastName:data.LastName,
-          UserEmail:data.UserEmail,
-          UserPass:data.UserPass,
-          ProjectID:data.ProjectID,
-          role:'65ad12f317279bae27eb5943',
-          status:data.status
-        }
-        console.log(object)
-        const res = await axios.post("http://localhost:4000/api/user", object);
-        console.log(res.data.data);
-        navigate("/admin/adminprofile");
-      } catch (error) {
-        console.log(
-          "Failed to submit project:",
-          error.response ? error.response.data : error.message
-        );
-      }
-    };
+    if (isChecked) {
+      // Add the project ID if the checkbox is checked
+      setSelectedProjects([...selectedProjects, projectId]);
+    } else {
+      // Remove the project ID if the checkbox is unchecked
+      setSelectedProjects(selectedProjects.filter(id => id !== projectId));
+    }
+  };
   return (
     <div className="content">
-    <div className="container-fluid">
-      <form onSubmit={handleSubmit(submitHandler)}>
-        <div className="row">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              width: "100%",
-            }}
-          >
-            <div>
-              <div className="card">
-                <div className="card-header" style={{ textAlign: "center" }}>
-                  <h3
-                    className="card-title"
-                    style={{
-                      fontFamily: "Arial, sans-serif",
-                      fontWeight: "bold",
-                      color: "#34568B",
-                    }}
-                  >
-                    Add User
-                  </h3>
-                  <Link
-            type="button"
-            to="/admin/adminprofile"
-            rel="tooltip"
-            className="btn btn-danger btn-simple btn-link"
-            data-original-title="Remove"
-            style={{
-              display: "flex",
-              alignItems: "right",
-              fontSize: "17px",
-              position: "relative",
-              left: "96%",
-              top: "-44px",
-            }}
-          >
-            <i className="fa fa-times" />
-          </Link>
-                </div>
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-md-6 pr-1">
-                      <div className="form-group">
-                        <label>First Name</label>
-                        <input
-                          type="text"
-                          {...register("FirstName")}
-                          className="form-control"
-                          placeholder="First Name"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-md-6 pl-1">
-                      <div className="form-group">
-                        <label>Last Name</label>
-                        <input
-                          type="text"
-                          {...register("LastName")}
-                          className="form-control"
-                          placeholder="Last Name"
-                        />
-                      </div>
-                    </div>
+      <div className="container-fluid">
+        <form onSubmit={handleSubmit(submitHandler)}>
+          <div className="row">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                width: "100%",
+              }}
+            >
+              <div>
+                <div className="card">
+                  <div className="card-header" style={{ textAlign: "center" }}>
+                    <h3
+                      className="card-title"
+                      style={{
+                        fontFamily: "Arial, sans-serif",
+                        fontWeight: "bold",
+                        color: "#34568B",
+                      }}
+                    >
+                      Add User
+                    </h3>
+                    <Link
+                      type="button"
+                      to="/admin/adminprofile"
+                      rel="tooltip"
+                      className="btn btn-danger btn-simple btn-link"
+                      data-original-title="Remove"
+                      style={{
+                        display: "flex",
+                        alignItems: "right",
+                        fontSize: "17px",
+                        position: "relative",
+                        left: "96%",
+                        top: "-44px",
+                      }}
+                    >
+                      <i className="fa fa-times" />
+                    </Link>
                   </div>
-                  <div className="row">
-                    <div className="col-md-6 pr-1">
-                      <div className="form-group">
-                        <label>User Email</label>
-                        <input
-                          type="text"
-                          {...register("UserEmail")}
-                          className="form-control"
-                          placeholder="User Email"
-                        />
+                  <div className="card-body">
+                    <div className="row">
+                      <div className="col-md-6 pr-1">
+                        <div className="form-group">
+                          <label>First Name</label>
+                          <input
+                            type="text"
+                            {...register("FirstName")}
+                            className="form-control"
+                            placeholder="First Name"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-6 pl-1">
+                        <div className="form-group">
+                          <label>Last Name</label>
+                          <input
+                            type="text"
+                            {...register("LastName")}
+                            className="form-control"
+                            placeholder="Last Name"
+                          />
+                        </div>
                       </div>
                     </div>
-                    <div className="col-md-6 pl-1">
-                      <div className="form-group">
-                        <label>User Pass</label>
-                        <input
-                          type="text"
-                          {...register("UserPass")}
-                          className="form-control"
-                          placeholder="User Password"
-                        />
+                    <div className="row">
+                      <div className="col-md-6 pr-1">
+                        <div className="form-group">
+                          <label>User Email</label>
+                          <input
+                            type="text"
+                            {...register("UserEmail")}
+                            className="form-control"
+                            placeholder="User Email"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-6 pl-1">
+                        <div className="form-group">
+                          <label>User Pass</label>
+                          <input
+                            type="text"
+                            {...register("UserPass")}
+                            className="form-control"
+                            placeholder="User Password"
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  {/* <div className="row">
+                    {/* <div className="row">
                     <div className="col-md-6 pr-1">
                       <div className="form-group">
                         <label>Latitude</label>
@@ -182,8 +194,8 @@ const CreateUser = () => {
                       </div>
                     </div>
                   </div> */}
-                  <div className="row">
-                    {/* <div className="col-md-12">
+                    <div className="row">
+                      {/* <div className="col-md-12">
                       <div className="form-group">
                         <label>Role</label>
                         <input
@@ -195,78 +207,91 @@ const CreateUser = () => {
                         />
                       </div>
                     </div> */}
-                  </div>
-                  <div className="row">
-                    <div className="col-md-6 pr-1">
-                      <div className="form-group">
-                        <label>Project Enroll</label>
-                        <select
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6 pr-1">
+                        <div className="form-group">
+                          <label>Project Enroll</label>
+                          {/* <select
                           {...register("ProjectID")}
                           className="form-control"
+                          multiple
+                          style={{ height: "auto" }}
                         >
                           {project.map((pro) => (
                             <option key={pro._id} value={pro._id}>
                               {pro.ProjectName}
                             </option>
                           ))}
-                        </select>
+                        </select> */}
+                          {project.map((pro) => (
+                            <div key={pro._id}>
+                              <input
+                                type="checkbox"
+                                value={pro._id}
+                                checked={selectedProjects.includes(pro._id)}
+                                onChange={handleProjectChange}
+                              />
+                              {pro.ProjectName}
+                            </div>
+                          ))}
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-6 pl-1">
-                      <div className="form-group">
-                        <label>Status</label>
-                        <select
-                          {...register("status")}
-                          className="form-control"
-                        >
+                      <div className="col-md-6 pl-1">
+                        <div className="form-group">
+                          <label>Status</label>
+                          <select
+                            {...register("status")}
+                            className="form-control"
+                          >
                             <option value="">Select Status</option>
                             <option value="true">True</option>
                             <option value="false">False</option>
-                        </select>
+                          </select>
+                        </div>
                       </div>
                     </div>
+                    <button
+                      type="submit"
+                      className="btn btn-info btn-fill pull-right"
+                    >
+                      Submit
+                    </button>
                   </div>
-                  <button
-                    type="submit"
-                    className="btn btn-info btn-fill pull-right"
-                  >
-                    Submit
-                  </button>
                 </div>
               </div>
             </div>
+            <hr />
           </div>
-          <hr />
+        </form>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+        <div className="button-container">
+          <button
+            href="#"
+            className="btn btn-simple btn-link btn-icon"
+            style={{ fontSize: "25px", margin: "0 5px" }}
+          >
+            <FontAwesomeIcon icon={faFacebookSquare} />
+          </button>
+          <button
+            href="#"
+            className="btn btn-simple btn-link btn-icon"
+            style={{ fontSize: "25px", margin: "0 5px" }}
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </button>
+          <button
+            href="#"
+            className="btn btn-simple btn-link btn-icon"
+            style={{ fontSize: "25px", margin: "0 5px" }}
+          >
+            <FontAwesomeIcon icon={faGooglePlusSquare} />
+          </button>
         </div>
-      </form>
-    </div>
-    <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
-      <div className="button-container">
-        <button
-          href="#"
-          className="btn btn-simple btn-link btn-icon"
-          style={{ fontSize: "25px", margin: "0 5px" }}
-        >
-          <FontAwesomeIcon icon={faFacebookSquare} />
-        </button>
-        <button
-          href="#"
-          className="btn btn-simple btn-link btn-icon"
-          style={{ fontSize: "25px", margin: "0 5px" }}
-        >
-          <FontAwesomeIcon icon={faTwitter} />
-        </button>
-        <button
-          href="#"
-          className="btn btn-simple btn-link btn-icon"
-          style={{ fontSize: "25px", margin: "0 5px" }}
-        >
-          <FontAwesomeIcon icon={faGooglePlusSquare} />
-        </button>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default CreateUser
+export default CreateUser;
